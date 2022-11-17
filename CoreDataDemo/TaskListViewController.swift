c72e62d5bbdafeec876986fb39854a69f79875e8
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-11-17 16:03:41 +0400

    create TaskViewController

--- CoreDataDemo/TaskListViewController.swift
+++ CoreDataDemo/TaskListViewController.swift
@@ -43,7 +43,8 @@ class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        
+        let taskVC = TaskViewController()
+        present(taskVC, animated: true)
     }
 }
 
