c9763b36e33d1527e6c19d34fd71cf4d0d19c3cf
NKhristolyubov <n.khristolyubov@yandex.ru>
2022-11-18 12:02:36 +0400

    prepare TaskViewController

--- CoreDataDemo/TaskListViewController.swift
+++ CoreDataDemo/TaskListViewController.swift
@@ -8,43 +8,46 @@
 import UIKit
 
 class TaskListViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        setupNavigationBar()
-    }
+        setupNavigatoinBar()
 
-    private func setupNavigationBar() {
+    }
+    
+    private func setupNavigatoinBar() {
         title = "Task List"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithOpaqueBackground()
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        
-        navBarAppearance.backgroundColor = UIColor(
+        let navBarAppearence = UINavigationBarAppearance() // экземпляр класса, который настраивает внешний вид navigationBar
+        navBarAppearence.configureWithOpaqueBackground()
+        navBarAppearence.titleTextAttributes = [.foregroundColor: UIColor.white] // задаем цвет текста у заголовка
+        navBarAppearence.largeTitleTextAttributes = [.foregroundColor: UIColor.white] // задаем цвет текста у большого заголовка
+        navBarAppearence.backgroundColor = UIColor(
             red: 21/255,
             green: 101/255,
             blue: 192/255,
-            alpha: 194/255)
+            alpha: 194/255) // задаем цвет для NavBara
         
-        navigationController?.navigationBar.standardAppearance = navBarAppearance
-        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationController?.navigationBar.standardAppearance = navBarAppearence // применяем настройки, которые устанавливали выше, для НавБара во время скроллинга
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearence // применяем настройки, которые устанавливали выше, для НавБара в обычном состоянии
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
+        navigationItem.rightBarButtonItem = UIBarButtonItem( // добавляем кнопку на навБаре
             barButtonSystemItem: .add,
-            target: self,
-            action: #selector(addNewTask)
-            )
+            target: self, // где будет применяться
+            action: #selector(addNewTask) // действие по нажатию на кнопку
+        )
         
-        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.tintColor = .white // Цвет оттенка, применяемый к элементам навигации и элементам кнопки панели
     }
     
     @objc private func addNewTask() {
-        let taskVC = TaskViewController()
-        present(taskVC, animated: true)
+        
+        let newTaskVC = TaskViewController() // создаем экземпляр класса, на который будем переходить
+        present(newTaskVC, animated: true) // указываем куда будем переходить
     }
+    
 }
+
 
--- CoreDataDemo/TaskViewController.swift
+++ CoreDataDemo/TaskViewController.swift
@@ -8,23 +8,86 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    
+    private lazy var taskTextField: UITextField = {
+        let textFitld = UITextField()
+        
+        textFitld.placeholder = "New Task"
+        textFitld.borderStyle = .roundedRect
+        
+        return textFitld
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        createButton(
+            withColor: UIColor(
+                red: 21/255,
+                green: 101/255,
+                blue: 192/255,
+                alpha: 194/255),
+            andTitle: "Save task",
+            andAction: UIAction {_ in
+                self.dismiss(animated: true)
+            })
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        createButton(
+            withColor: .systemRed,
+            andTitle: "Cancel",
+            andAction: UIAction {_ in
+                self.dismiss(animated: true)
+            })
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-
-        // Do any additional setup after loading the view.
+        setup(subviews: taskTextField, saveButton, cancelButton)
+        setConstrains() // сначала устанавливается элемент интерфейса, а потом задаются каонстрейнты для него (последовательность имеет значение!)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setup(subviews: UIView...) { // размещаем сабвью на супервью
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
     }
-    */
-
+    
+    private func setConstrains() { // уствнавливаем констрейнты
+        taskTextField.translatesAutoresizingMaskIntoConstraints = false // отключаем автоматическую расстановку элементов для текстового поля(будем делать это вручную)
+        
+        NSLayoutConstraint.activate([ // устанавливаем констрейнты
+            taskTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([ // устанавливаем констрейнты
+            saveButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor, constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([ // устанавливаем констрейнты
+            cancelButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor, constant: 20),
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+    }
+    
+    private func createButton(withColor color: UIColor, andTitle title: String, andAction action: UIAction) -> UIButton {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = color
+        
+        var atributes = AttributeContainer() // создаем переменную с атрибутами для кнопки
+        
+        atributes.font = UIFont.boldSystemFont(ofSize: 18) // делаем жирный шрифт с опр.размером
+        buttonConfiguration.attributedTitle = AttributedString(title, attributes: atributes)
+        
+        return UIButton(configuration: buttonConfiguration, primaryAction: action)
+    }
 }
